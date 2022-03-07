5aaaa0e4313568b7cba396a6bb145c25e73fb0f9
Sly480 <ly.sergio480@gmail.com>
2022-03-07 14:45:24 -0500

    TODOs updated

--- server/src/core/net/session.rs
+++ server/src/core/net/session.rs
@@ -22,18 +22,3 @@ fn create_session(connection: TcpStream) -> Session {
         session_type: SessionType::BIND,
     };
 }
-
-// TODO: Transfer session to new ip
-fn transfer_session(session: Session, new_ip: IpAddr, session_type: SessionType) -> Session {
-    // TODO: Call create_bind_session or create_reverse_session based on session_type
-    return Session {
-        connection: session.connection,
-        session_type: session_type,
-    };
-}
-
-// TODO: Return TcpStream for bind connection
-// fn create_bind_session() -> TcpStream {}
-
-// TODO: Return TcpStream for reverse connection
-// fn create_reverse_session() -> TcpStream {}
